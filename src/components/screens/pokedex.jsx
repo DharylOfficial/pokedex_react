9d1658f63ee242fdbb8a14efcc7d0191049da8b5
= <=>
2022-01-24 00:19:41 +0800

    Installed ChakraProvider

---
+++ src/components/screens/pokedex.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+class Pokedex extends Component {
+  state = {};
+  render() {
+    return <div>This is a pokedex page</div>;
+  }
+}
+
+export default Pokedex;
