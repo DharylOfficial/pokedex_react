4de50e30e5194002325e4f8128e05929be0d05b9
= <=>
2022-01-23 23:37:44 +0800

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import Playground from "./components/playground";
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
